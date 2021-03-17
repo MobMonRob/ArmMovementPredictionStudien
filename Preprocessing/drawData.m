c0177622d2f645b58db70c9a9273625ad253f69e
Bender <nicolas.bender@sap.com>
2021-03-17 19:38:53 +0100

    Errorhandling when Left or Right file is missing

--- Preprocessing/drawData.m
+++ Preprocessing/drawData.m
@@ -5,8 +5,8 @@ function drawData(csvNumber,phaseNumber);
   phase='';
   switch (phaseNumber)
     case 0
-      csvPathLeft = ["../DATA/0_raw/" num2str(csvNumber) "_takeover_LGraspPhase.csv"];
-      csvPathRight = ["../DATA/0_raw/" num2str(csvNumber) "_takeover_RGraspPhase.csv"];
+      csvPathLeft = ["./0_raw/" num2str(csvNumber) "_takeover_LGraspPhase.csv"];
+      csvPathRight = ["./0_raw/" num2str(csvNumber) "_takeover_RGraspPhase.csv"];
       phase='raw';
     case 1
       phase = 'interpolated';
@@ -22,17 +22,42 @@ function drawData(csvNumber,phaseNumber);
       phase = 'interpolated';
     endswitch
   if (phaseNumber != 0)
-    csvPathLeft = ["../DATA/" num2str(phaseNumber) "_" phase "/" num2str(csvNumber) "_" phase "_L.csv"];
-    csvPathRight = ["../DATA/" num2str(phaseNumber) "_" phase "/" num2str(csvNumber) "_" phase "_R.csv"];
+    csvPathLeft = ["./" num2str(phaseNumber) "_" phase "/" num2str(csvNumber) "_" phase "_L.csv"];
+    csvPathRight = ["./" num2str(phaseNumber) "_" phase "/" num2str(csvNumber) "_" phase "_R.csv"];
   endif
 
-  L = dlmread(csvPathLeft, ";");
-  L = L(2:end,:); #remove first line of zeros
-  R = dlmread(csvPathRight, ";");
-  R = R(2:end,:);
-  plot3(R(:,1),R(:,2),R(:,3), ";Handgelenk rechts;",R(:,4),R(:,5),R(:,6), ";Ellenbogen rechts;",R(:,7),R(:,8),R(:,9), ";Schulter rechts;",L(:,1),L(:,2),L(:,3), ";Handgelenk links;",L(:,4),L(:,5),L(:,6), ";Ellenbogen links;",L(:,7),L(:,8),L(:,9), ";Schulter links;");
+  error_L = false;
+  error_R = false;
+  
+  try
+    L = dlmread(csvPathLeft, ";");
+    L = L(2:end,:); #remove first line of zeros
+  catch
+    error_L = true;
+  end_try_catch
+  
+  try
+    R = dlmread(csvPathRight, ";");
+    R = R(2:end,:);
+  catch
+    error_R = true;
+  end_try_catch
+
+  if(error_R && error_L)
+    disp("Error in both datasets, maybe wrong filename") 
+  endif
+
+  if(error_L)
+    plot3(R(:,1),R(:,2),R(:,3), ";Handgelenk rechts;",R(:,4),R(:,5),R(:,6), ";Ellenbogen rechts;",R(:,7),R(:,8),R(:,9), ";Schulter rechts;");
+  elseif(error_R)
+    plot3(L(:,1),L(:,2),L(:,3), ";Handgelenk links;",L(:,4),L(:,5),L(:,6), ";Ellenbogen links;",L(:,7),L(:,8),L(:,9), ";Schulter links;");
+  else
+    plot3(R(:,1),R(:,2),R(:,3), ";Handgelenk rechts;",R(:,4),R(:,5),R(:,6), ";Ellenbogen rechts;",R(:,7),R(:,8),R(:,9), ";Schulter rechts;",L(:,1),L(:,2),L(:,3), ";Handgelenk links;",L(:,4),L(:,5),L(:,6), ";Ellenbogen links;",L(:,7),L(:,8),L(:,9), ";Schulter links;");
+  endif
+  
   title(strcat(num2str(csvNumber), "-", phase)); #set title
   xlabel('x');
   ylabel('y');
   zlabel('z');
 end
+
