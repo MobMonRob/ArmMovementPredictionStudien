d3f98403c885de64219f7b8dccca62df9bcf55de
Philipp <online@philipp-rall.de>
2021-04-11 23:11:54 +0200

    Improve drawPoints

--- Preprocessing/drawPoints.m
+++ Preprocessing/drawPoints.m
@@ -1,11 +1,14 @@
 # e.g. drawPoints('endpoints_42')
 
-function drawPoints(filenumber)
-    csvPath = ["../DATA/97_endpoints/" filenumber ".csv"];
+function drawPoints(side)
+    csvPath1 = ["../DATA/97_endpoints/endpoints_0305_" side ".csv"];
+    csvPath2 = ["../DATA/97_endpoints/endpoints_0403_" side ".csv"];
+    csvPath3 = ["../DATA/97_endpoints/endpoints_0423_" side ".csv"];
     error = false;
     try
-      D = dlmread(csvPath, ";");
-      D = D(2:end,:); #remove first line of zeros
+      D1 = dlmread(csvPath1, ";");
+      D2 = dlmread(csvPath2, ";");
+      D3 = dlmread(csvPath3, ";");
     catch
       error = true;
     end_try_catch
@@ -14,8 +17,10 @@ function drawPoints(filenumber)
         disp("Error in dataset, maybe wrong filename") 
     endif
 
-    scatter3 (D(:,1), D(:,2), D(:,3))
-    title(filenumber));
+    scatter3 (D1(:,1), D1(:,2), D1(:,3));
+    hold on;
+    scatter3 (D2(:,1), D2(:,2), D2(:,3));
+    scatter3 (D3(:,1), D3(:,2), D3(:,3));
 
   
   
