0ea72c2f3706e30cfe8c05f97497e9bfb9b68e8b
Nicolas Bender <nicolas.bender@posteo.de>
2021-04-17 05:35:41 -0700

    Changed and added smooth method to savgol

--- Preprocessing/smooth/check_different_window_sizes.py
+++ Preprocessing/smooth/check_different_window_sizes.py
@@ -1,10 +1,99 @@
 import os
 
+from numpy import sqrt
+from numpy.random import shuffle
+
 from ArmMovementPredictionStudien.Preprocessing.smooth import smooth_data_utils
 
-directory = "../../DATA/1_interpolated/"
+import matplotlib.pyplot as plt
 
-for file in os.listdir(directory):
-    smoothed_dataset = smooth_data_utils.generate_smooth_dataframe(file, directory, window=5)
-    new_filename = "./wind_5/" + file.replace("interpolated", "smoothed")
-    smoothed_dataset.to_csv(path_or_buf=new_filename, sep=';', index=False, float_format='%.4f', na_rep='NaN')
+from ArmMovementPredictionStudien.Preprocessing.utils.velocity import calculate_velocity_vector_for_dataset_filename
+
+
+def generate_smooth_data():
+    directory = "../../DATA/1_interpolated/"
+    for file in os.listdir(directory):
+        for i in [3, 5, 7, 9, 11, 13, 15]:
+            smoothed_dataset = smooth_data_utils.generate_smooth_dataframe(file, directory, "savgol", window=i)
+            new_filename = f"./wind_{i}/" + file.replace("interpolated", "smoothed")
+            smoothed_dataset.to_csv(path_or_buf=new_filename, sep=';', index=False, float_format='%.4f', na_rep='NaN')
+
+
+def show_histograms_with_vmax():
+    fig, axs = plt.subplots(2, 4)
+
+    directory = "../../DATA/1_interpolated/"
+    v_max_list = {}
+    for file in os.listdir(directory):
+        v_vector = calculate_velocity_vector_for_dataset_filename(file, directory)
+        v_trajectory = sqrt(v_vector[:, 0] ** 2 + v_vector[:, 1] ** 2 + v_vector[:, 2] ** 2)
+        v_max = max(v_trajectory)
+        if v_max > 800:
+            print(f"v_max is {v_max} in file {file}")
+        else:
+            v_max_list.update({file: v_max})
+    axs[0, 0].hist(v_max_list.values())
+    axs[0, 0].set_title(f"raw")
+
+    for i in [3, 5, 7, 9, 11, 13, 15]:
+        directory = f"./wind_{i}/"
+        v_max_list = {}
+        for file in os.listdir(directory):
+            v_vector = calculate_velocity_vector_for_dataset_filename(file, directory)
+            v_trajectory = sqrt(v_vector[:, 0] ** 2 + v_vector[:, 1] ** 2 + v_vector[:, 2] ** 2)
+            v_max = max(v_trajectory)
+            if v_max > 100:
+                pass
+                # print(f"v_max is {v_max} in file {file}")
+            else:
+                v_max_list.update({file: v_max})
+        if i == 3:
+            axs[0, 1].hist(v_max_list.values())
+            axs[0, 1].set_title(f"window = {i}")
+        elif i == 5:
+            axs[0, 2].hist(v_max_list.values())
+            axs[0, 2].set_title(f"window = {i}")
+        elif i == 7:
+            axs[0, 3].hist(v_max_list.values())
+            axs[0, 3].set_title(f"window = {i}")
+        elif i == 9:
+            axs[1, 0].hist(v_max_list.values())
+            axs[1, 0].set_title(f"window = {i}")
+        elif i == 11:
+            axs[1, 1].hist(v_max_list.values())
+            axs[1, 1].set_title(f"window = {i}")
+        elif i == 13:
+            axs[1, 2].hist(v_max_list.values())
+            axs[1, 2].set_title(f"window = {i}")
+        elif i == 15:
+            axs[1, 3].hist(v_max_list.values())
+            axs[1, 3].set_title(f"window = {i}")
+    plt.show()
+
+
+def show_examples_of_velocities(file="rand"):
+    dir_int = "../../DATA/1_interpolated/"
+    if file == "rand":
+        files = os.listdir(dir_int)
+        shuffle(files)
+        file = files[0]
+    v_vector = calculate_velocity_vector_for_dataset_filename(file, dir_int)
+    v_trajectory = sqrt(v_vector[:, 0] ** 2 + v_vector[:, 1] ** 2 + v_vector[:, 2] ** 2)
+    plt.plot(v_trajectory, label=f"raw")
+    plt.title(file)
+    file = file.replace("interpolated", "smoothed")
+    for i in [3, 5, 7, 9, 11, 13, 15]:
+        dir_smooth = f"./wind_{i}/"
+        v_vector = calculate_velocity_vector_for_dataset_filename(file, dir_smooth)
+        v_trajectory = sqrt(v_vector[:, 0] ** 2 + v_vector[:, 1] ** 2 + v_vector[:, 2] ** 2)
+        plt.plot(v_trajectory, label=f"window={i}")
+    print(file)
+    plt.legend(loc='best')
+    plt.show()
+
+
+if __name__ == "__main__":
+    generate_smooth_data()
+    # show_histograms_with_vmax()
+    # show_examples_of_velocities("20200423134_interpolated_L.csv")
+    show_examples_of_velocities("20200423307_interpolated_R.csv")
--- Preprocessing/smooth/smooth_data_utils.py
+++ Preprocessing/smooth/smooth_data_utils.py
@@ -1,17 +1,32 @@
+import math
+
 from numpy import median
 import numpy as np
 import pandas as pd
+from numpy import mean, median
+from scipy.signal import savgol_filter
 
 from ArmMovementPredictionStudien.Preprocessing.utils import velocity
 from ArmMovementPredictionStudien.Preprocessing.utils.utils import open_dataset_numpy
 
 
-def filter_dataset(filename, directory, window=5):
+def filter_dataset(filename, directory, method, window=7):
     dataset = open_dataset_numpy(filename, directory)
-    return median_filter(dataset, window)
+    if method == "median":
+        return median_filter(dataset, window)
+    elif method == "mean":
+        return mean_filter(dataset, window)
+    elif method == "sma":
+        return sma_filter(dataset, window)
+    elif method == "dynamic":
+        return dynamic_filter(dataset, window)
+    elif method == "savgol":
+        return savitzky_golay(dataset, window)
+    else:
+        raise Exception("Choose a filter method: 'median', 'mean', 'sma', 'savgol'")
 
 
-def generate_smooth_dataframe(filename, directory, window=5):
+def generate_smooth_dataframe(filename, directory, method, window=7):
     """
     Generates a smoothed Pandas Dataframe from a dataset by its filename.
     This smooths only the position data, not the velocity!
@@ -22,7 +37,7 @@ def generate_smooth_dataframe(filename, directory, window=5):
      7 the next three values
     :return: returns a dataframe of smoothed position-data
     """
-    dataset_smooth = filter_dataset(filename, directory, window)
+    dataset_smooth = filter_dataset(filename, directory, method, window)
     right_or_left = velocity.determine_left_or_right_dataset(filename)
     column_names = [
         "{r_l}WJC_x".format(r_l=right_or_left),
@@ -42,45 +57,244 @@ def generate_smooth_dataframe(filename, directory, window=5):
     return dataframe_smooth
 
 
-def median_filter(dataset, window=5):
+def mean_filter(dataset, window=7):
+    return window_before_and_after(dataset, "mean", window=window)
+
+
+def median_filter(dataset, window=7):
+    return window_before_and_after(dataset, "median", window=window)
+
+
+def sma_filter(dataset, window):
+    return window_before(dataset, window=window)
+
+
+def dynamic_filter(dataset, window):
+    return window_dynamic(dataset, window=window)
+
+
+def window_before_and_after(dataset, method, window=7):
     last_index = len(dataset) - 1
     filtered_dataset = np.empty(shape=(len(dataset), 9))
+
     if window == 3:
-        filtered_dataset[0] = dataset[0]
-        filtered_dataset[last_index] = dataset[last_index]
+        filtered_dataset[0] = float('nan')
+        filtered_dataset[last_index] = float('nan')
 
         for i in range(1, len(dataset) - 1):
-            values = [dataset[i - 1], dataset[i],
+            values = [dataset[i - 1],
+                      dataset[i],
                       dataset[i + 1]]
-            median_number = median(values, axis=0)
-            filtered_dataset[i] = median_number
+            if method == "median":
+                average = median(values, axis=0)
+            elif method == "mean":
+                average = mean(values, axis=0)
+            else:
+                average = 0
+            filtered_dataset[i] = average
 
     elif window == 5:
-        filtered_dataset[0] = dataset[0]
-        filtered_dataset[1] = dataset[1]
-        filtered_dataset[last_index - 1] = dataset[last_index - 1]
-        filtered_dataset[last_index] = dataset[last_index]
+        filtered_dataset[0] = float('nan')
+        filtered_dataset[1] = float('nan')
+        filtered_dataset[last_index - 1] = float('nan')
+        filtered_dataset[last_index] = float('nan')
 
         for i in range(2, len(dataset) - 2):
-            values = [dataset[i - 2], dataset[i - 1], dataset[i],
+            values = [dataset[i - 2], dataset[i - 1],
+                      dataset[i],
                       dataset[i + 1], dataset[i + 2]]
-            median_number = median(values, axis=0)
-            filtered_dataset[i] = median_number
+            if method == "median":
+                average = median(values, axis=0)
+            elif method == "mean":
+                average = mean(values, axis=0)
+            else:
+                average = 0
+            filtered_dataset[i] = average
 
     elif window == 7:
-        filtered_dataset[0] = dataset[0]
-        filtered_dataset[1] = dataset[1]
-        filtered_dataset[2] = dataset[2]
-        filtered_dataset[last_index - 2] = dataset[last_index - 2]
-        filtered_dataset[last_index - 1] = dataset[last_index - 1]
-        filtered_dataset[last_index] = dataset[last_index]
+        filtered_dataset[0] = float('nan')
+        filtered_dataset[1] = float('nan')
+        filtered_dataset[2] = float('nan')
+        filtered_dataset[last_index - 2] = float('nan')
+        filtered_dataset[last_index - 1] = float('nan')
+        filtered_dataset[last_index] = float('nan')
 
         for i in range(3, len(dataset) - 3):
-            values = [dataset[i - 3], dataset[i - 2], dataset[i - 1], dataset[i],
+            values = [dataset[i - 3], dataset[i - 2], dataset[i - 1],
+                      dataset[i],
                       dataset[i + 1], dataset[i + 2], dataset[i + 3]]
-            median_number = median(values, axis=0)
-            filtered_dataset[i] = median_number
+            if method == "median":
+                average = median(values, axis=0)
+            elif method == "mean":
+                average = mean(values, axis=0)
+            else:
+                average = 0
+            filtered_dataset[i] = average
+
+    elif window == 9:
+        filtered_dataset[0] = float('nan')
+        filtered_dataset[1] = float('nan')
+        filtered_dataset[2] = float('nan')
+        filtered_dataset[3] = float('nan')
+        filtered_dataset[last_index - 3] = float('nan')
+        filtered_dataset[last_index - 2] = float('nan')
+        filtered_dataset[last_index - 1] = float('nan')
+        filtered_dataset[last_index] = float('nan')
+
+        for i in range(4, len(dataset) - 4):
+            values = [dataset[i - 4], dataset[i - 3], dataset[i - 2], dataset[i - 1],
+                      dataset[i],
+                      dataset[i + 1], dataset[i + 2], dataset[i + 3], dataset[i + 4]]
+            if method == "median":
+                average = median(values, axis=0)
+            elif method == "mean":
+                average = mean(values, axis=0)
+            else:
+                average = 0
+            filtered_dataset[i] = average
+
+    elif window == 11:
+        filtered_dataset[0] = float('nan')
+        filtered_dataset[1] = float('nan')
+        filtered_dataset[2] = float('nan')
+        filtered_dataset[3] = float('nan')
+        filtered_dataset[4] = float('nan')
+        filtered_dataset[last_index - 4] = float('nan')
+        filtered_dataset[last_index - 3] = float('nan')
+        filtered_dataset[last_index - 2] = float('nan')
+        filtered_dataset[last_index - 1] = float('nan')
+        filtered_dataset[last_index] = float('nan')
+
+        for i in range(5, len(dataset) - 5):
+            values = [dataset[i - 5], dataset[i - 4], dataset[i - 3], dataset[i - 2], dataset[i - 1],
+                      dataset[i],
+                      dataset[i + 1], dataset[i + 2], dataset[i + 3], dataset[i + 4], dataset[i + 5]]
+            if method == "median":
+                average = median(values, axis=0)
+            elif method == "mean":
+                average = mean(values, axis=0)
+            else:
+                average = 0
+            filtered_dataset[i] = average
+
+    elif window == 13:
+        filtered_dataset[0] = float('nan')
+        filtered_dataset[1] = float('nan')
+        filtered_dataset[2] = float('nan')
+        filtered_dataset[3] = float('nan')
+        filtered_dataset[4] = float('nan')
+        filtered_dataset[5] = float('nan')
+        filtered_dataset[last_index - 5] = float('nan')
+        filtered_dataset[last_index - 4] = float('nan')
+        filtered_dataset[last_index - 3] = float('nan')
+        filtered_dataset[last_index - 2] = float('nan')
+        filtered_dataset[last_index - 1] = float('nan')
+        filtered_dataset[last_index] = float('nan')
+
+        for i in range(6, len(dataset) - 6):
+            values = [dataset[i - 6], dataset[i - 5], dataset[i - 4], dataset[i - 3], dataset[i - 2], dataset[i - 1],
+                      dataset[i],
+                      dataset[i + 1], dataset[i + 2], dataset[i + 3], dataset[i + 4], dataset[i + 5], dataset[i + 6]]
+            if method == "median":
+                average = median(values, axis=0)
+            elif method == "mean":
+                average = mean(values, axis=0)
+            else:
+                average = 0
+            filtered_dataset[i] = average
+
+    elif window == 15:
+        filtered_dataset[0] = float('nan')
+        filtered_dataset[1] = float('nan')
+        filtered_dataset[2] = float('nan')
+        filtered_dataset[3] = float('nan')
+        filtered_dataset[4] = float('nan')
+        filtered_dataset[5] = float('nan')
+        filtered_dataset[6] = float('nan')
+        filtered_dataset[last_index - 6] = float('nan')
+        filtered_dataset[last_index - 5] = float('nan')
+        filtered_dataset[last_index - 4] = float('nan')
+        filtered_dataset[last_index - 3] = float('nan')
+        filtered_dataset[last_index - 2] = float('nan')
+        filtered_dataset[last_index - 1] = float('nan')
+        filtered_dataset[last_index] = float('nan')
+
+        for i in range(7, len(dataset) - 7):
+            values = [dataset[i - 7], dataset[i - 6], dataset[i - 5], dataset[i - 4], dataset[i - 3], dataset[i - 2],
+                      dataset[i - 1],
+                      dataset[i],
+                      dataset[i + 1],
+                      dataset[i + 2], dataset[i + 3], dataset[i + 4], dataset[i + 5], dataset[i + 6], dataset[i + 7]]
+            if method == "median":
+                average = median(values, axis=0)
+            elif method == "mean":
+                average = mean(values, axis=0)
+            else:
+                average = 0
+            filtered_dataset[i] = average
 
     else:
-        raise Exception("Window has to be 3 or 5 or 7")
+        raise Exception("Window has to be 3; 5; 7; 9; 11; 13 or 15")
     return filtered_dataset
+
+
+def window_before(dataset, window=7):
+    filtered_dataset = np.empty(shape=(len(dataset), 9))
+
+    if len(dataset) <= window:
+        raise Exception(f"Dataset is to short: {len(dataset)} entries.")
+
+    if 0 <= window < 16:
+        for i in range(0, window):
+            filtered_dataset[i] = dataset[i]
+
+        for i in range(window, len(dataset)):
+            values = [dataset[i]]
+            for j in range(0, window):
+                values.append(dataset[i - j - 1])
+            average = mean(values, axis=0)
+            filtered_dataset[i] = average
+    else:
+        raise Exception("Window has to be in [0; 15]")
+
+    return filtered_dataset
+
+
+def window_dynamic(dataset, window=7):
+    print("DO NOT USE THIS FILTER, SOME VALUES ARE BEING CALCULATED TWICE BECAUSE OF THE DYNAMIC WINDOW")
+    filtered_dataset = np.empty(shape=(len(dataset), 9))
+
+    if len(dataset) <= window:
+        raise Exception(f"Dataset is to short: {len(dataset)} entries.")
+
+    if 0 < window > 16:
+        raise Exception("Window has to be in [0; 15]")
+
+    for index in range(0, len(dataset)):
+        p_rel = math.ceil((index * window) / len(dataset))
+        if p_rel < 0.1:
+            p_rel = 1
+        if p_rel > window:
+            p_rel = window
+        lower_bound = p_rel - 1
+        upper_bound = window - p_rel
+
+        values = []
+        for j in range(index - lower_bound, index + upper_bound + 1):
+            values.append(dataset[j])
+        average = mean(values, axis=0)
+        filtered_dataset[index] = average
+
+    return filtered_dataset
+
+
+def savitzky_golay(dataset, window):
+    if window == 3 or 5:
+        order = 1
+    elif window == 7 or 9:
+        order = 2
+    elif window == 11 or 13:
+        order = 3
+    else:
+        order = 4
+    return savgol_filter(dataset, window, order, axis=0)
